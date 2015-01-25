8ba76f7118e92285ca6a7751679f8e19e0224213
lnbcoding <leonardbui@gmail.com>
2015-01-24 17:03:18 -0800

    change from 2 spaces to 4 spaces for my better readibility

--- source/dynamic-todos/public/js/application.js
+++ source/dynamic-todos/public/js/application.js
@@ -1,33 +1,33 @@
 $(document).ready(function() {
-  bindEvents();
+    bindEvents();
 });
 
 
 function bindEvents() {
-  // Bind functions which add, remove, and complete todos to the appropriate
-  // elements
+    $("form").on("submit", addTodo)
+
 }
 
 function buildTodo(todoName) {
-  // gets todoTemplate stored in DOM.
-  var todoTemplate = $.trim($('#todo_template').html());
-  // Creates an jQueryDOMElement from the todoTemplate.
-  var $todo = $(todoTemplate);
-  // Modifies it's text to use the passed in todoName.
-  $todo.find('h2').text(todoName);
-  // Returns the jQueryDOMElement to be used elsewhere.
-  return $todo;
+    // gets todoTemplate stored in DOM.
+    var todoTemplate = $.trim($('#todo_template').html());
+    // Creates an jQueryDOMElement from the todoTemplate.
+    var $todo = $(todoTemplate);
+    // Modifies it's text to use the passed in todoName.
+    $todo.find('h2').text(todoName);
+    // Returns the jQueryDOMElement to be used elsewhere.
+    return $todo;
 }
 
 function addTodo() {
-  event.preventDefault();
-  var $target = $(event.target) // which equals $(this) jquery object
-  $.ajax({
-    type: $target.attr("method"),
-    url: $target.attr("action"),
-    dataType: "json",
-    data: $target.serialize()
-  }).done(function(serverResponse) {
-    $(".todo_list").append(buildTodo(serverResponse.todo_content))
-  })
+    event.preventDefault();
+    var $target = $(event.target) // which equals $(this) jquery object
+    $.ajax({
+        type: $target.attr("method"),
+        url: $target.attr("action"),
+        dataType: "json",
+        data: $target.serialize()
+    }).done(function(serverResponse) {
+        $(".todo_list").append(buildTodo(serverResponse.todo_content))
+    })
 };
